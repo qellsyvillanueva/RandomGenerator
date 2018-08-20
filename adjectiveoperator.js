ab245b75d008d83454bdbe57341e491eea12a589
qellsyvillanueva <42422251+qellsyvillanueva@users.noreply.github.com>
2018-08-20 11:57:12 -1000

    Add files via upload

---
+++ adjectiveoperator.js
@@ -0,0 +1,12 @@
+var adjectives =
+      [ "conceited",
+        "ignorant",
+        "untalented"];
+        
+var person = prompt("What is your name?");
+
+var choose = (Math.floor(Math.random() * adjectives.length));
+
+var
+
+window.alert(total);
---
+++ foodchooser.js
@@ -0,0 +1,7 @@
+var foods = ["ramen",
+"tacos",
+"sushi"];
+
+var choose = (Math.floor(Math.random() * foods.length));
+
+window.alert(foods[choose]);
